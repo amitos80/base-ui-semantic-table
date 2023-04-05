8e5196b04b72aa2cf649b58e9e1f51c332ac535a
amitos80 <amitos80@gmail.com>
2023-04-05 09:41:04 +0300

    working example

---
+++ src/example.tsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+import { useState } from "react";
+import { Table, TableBuilderColumn, TableBuilder } from "baseui/table-semantic";
+// import {
+//   Table,
+
+// } from "baseui/table";
+
+import { Input, SIZE as INPUT_SIZE } from "baseui/input";
+//import { ArrowUp, ArrowDown } from "baseui/icon";
+import { LabelLarge } from "baseui/typography";
+
+type Row = {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+};
+
+type Column = {
+  id: string;
+  header: string;
+};
+
+const rows: Row[] = [
+  { id: 1, name: "John Doe", email: "john@example.com", age: 25 },
+  { id: 2, name: "Jane Doe", email: "jane@example.com", age: 30 },
+  { id: 3, name: "Bob Smith", email: "bob@example.com", age: 20 }
+];
+
+const columns: Column[] = [
+  { id: "name", header: "Name" },
+  { id: "email", header: "Email" },
+  { id: "age", header: "Age" }
+];
+
+const EnhancedTable = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortColumn, setSortColumn] = useState<string>("");
+  const [sortAscending, setSortAscending] = useState(true);
+
+  // Example data
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // const handleSort = (id: number) => {
+  //   if (sortColumn && sortColumn === String(id)) {
+  //     setSortAscending(!sortAscending);
+  //   } else {
+  //     setSortColumn(String(id));
+  //     setSortAscending(true);
+  //   }
+  // };
+
+  const filteredRows = rows.filter((row) => {
+    return Object.values(row).some((value) => {
+      return value.toString().toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  });
+
+  const sortedRows = [...filteredRows].sort((a: Row, b: Row) => {
+    if (sortColumn) {
+      const comparison =
+        a[sortColumn as keyof Row] > b[sortColumn as keyof Row] ? 1 : -1;
+      return sortAscending ? comparison : -comparison;
+    } else {
+      return 0;
+    }
+  });
+
+  // overrides?: BuilderOverrides;
+  // children?: React.ReactNode;
+  // data: Array<RowT>;
+  // divider?: Divider;
+  // horizontalScrollWidth?: string;
+  // sortColumn?: string | null;
+  // sortOrder?: 'ASC' | 'DESC' | null;
+  // onSort?: (columnId: string) => void;
+  // isLoading?: boolean;
+  // loadingMessage?: React.ReactNode | (() => React.ReactNode);
+  // emptyMessage?: React.ReactNode | (() => React.ReactNode);
+  // size?: Size;
+
+  return (
+    <div>
+      <LabelLarge>Search:</LabelLarge>
+      <Input
+        value={searchTerm}
+        onChange={(e: any) => handleSearch(e)}
+        size={INPUT_SIZE.compact}
+      />
+      <TableBuilder<Row>
+        data={sortedRows}
+      >
+        {columns.map((column, index) => (
+          <TableBuilderColumn<Row>
+            key={column.id}
+            id={column.id}
+            header={column.header}
+            //sort={column.id}
+          >
+            {(row: any) => <div>{row[column.id]}</div>}
+          </TableBuilderColumn>
+        ))}
+      </TableBuilder>
+    </div>
+  );
+};
+
+export default EnhancedTable;
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+///import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import Example from './example'
+
+const engine = new Styletron();
+
+function App() {
+    return (<Example />);
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+            <App />
+        </BaseProvider>
+    </StyletronProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
